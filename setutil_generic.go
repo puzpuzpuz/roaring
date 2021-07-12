56306171f5fbe8726d082c4a9dd02538933d252f
Andrey Pechkurov <apechkurov@gmail.com>
2021-07-12 11:34:00 +0300

    Introduce union2by2_branchless

--- setutil_generic.go
+++ setutil_generic.go
@@ -2,13 +2,106 @@
 
 package roaring
 
+import "unsafe"
 
-func union2by2(set1 []uint16, set2 []uint16, buffer []uint16) int {
+var (
+	k1table = [3]uint{1, 1, 0}
+	k2table = [3]uint{1, 0, 1}
+)
+
+func compareuint16(x, y uint16) int {
+	// branchless analog of the following code:
+	// if x == y {
+	// 	return 0
+	// }
+	// if x < y {
+	// 	return 1
+	// }
+	// return 2
+	bsmaller := (int(x) - int(y)) >> 63
+	bgreater := (int(y) - int(x)) >> 63
+	return int(bgreater*(-2) + bsmaller*(-1))
+}
+
+func compareuint(x, y uint) int {
+	bsmaller := (int(x) - int(y)) >> 63
+	bgreater := (int(y) - int(x)) >> 63
+	return int(bgreater*(-2) + bsmaller*(-1))
+}
+
+func uint16SlicePtr(slice []uint16, idx uint) *uint16 {
+	p := unsafe.Pointer(&slice[0])
+	indexp := (unsafe.Pointer)(uintptr(p) + 2*uintptr(idx))
+	return (*uint16)(indexp)
+}
+
+func uint16TableItem(arr [3]uint16, idx int) uint16 {
+	p := unsafe.Pointer(&arr[0])
+	indexp := (unsafe.Pointer)(uintptr(p) + 2*uintptr(idx))
+	return *(*uint16)(indexp)
+}
+
+func uintTableItem(arr [3]uint, idx int) uint {
+	p := unsafe.Pointer(&arr[0])
+	indexp := (unsafe.Pointer)(uintptr(p) + 8*uintptr(idx))
+	return *(*uint)(indexp)
+}
+
+func union2by2_branchless(set1 []uint16, set2 []uint16, buffer []uint16) int {
+	if 0 == len(set2) {
+		buffer = buffer[:len(set1)]
+		copy(buffer, set1[:])
+		return len(set1)
+	}
+	if 0 == len(set1) {
+		buffer = buffer[:len(set2)]
+		copy(buffer, set2[:])
+		return len(set2)
+	}
+	var (
+		s1, s2     uint16
+		nooverflow int = 1
+		stable     [3]uint16
+	)
 	pos := uint(0)
 	k1 := uint(0)
 	k2 := uint(0)
 	len1 := uint(len(set1))
 	len2 := uint(len(set2))
+	buffer = buffer[:len1+len2]
+	for nooverflow == 1 {
+		s1 = *uint16SlicePtr(set1, k1)
+		s2 = *uint16SlicePtr(set2, k2)
+		c := compareuint16(s1, s2)
+
+		stable[0] = s1
+		stable[1] = s1
+		stable[2] = s2
+		*uint16SlicePtr(buffer, pos) = uint16TableItem(stable, c)
+
+		pos++
+		k1 += uintTableItem(k1table, c)
+		k2 += uintTableItem(k2table, c)
+
+		nooverflow = compareuint(k1, len1) & compareuint(k2, len2)
+	}
+	if k1 >= len1 {
+		copy(buffer[pos:], set2[k2:])
+		pos += len2 - k2
+		return int(pos)
+	}
+	if k2 >= len2 {
+		copy(buffer[pos:], set1[k1:])
+		pos += len1 - k1
+		return int(pos)
+	}
+	return int(pos)
+}
+
+func union2by2(set1 []uint16, set2 []uint16, buffer []uint16) int {
+	pos := 0
+	k1 := 0
+	k2 := 0
 	if 0 == len(set2) {
 		buffer = buffer[:len(set1)]
 		copy(buffer, set1[:])
@@ -21,16 +114,16 @@ func union2by2(set1 []uint16, set2 []uint16, buffer []uint16) int {
 	}
 	s1 := set1[k1]
 	s2 := set2[k2]
-	buffer = buffer[:len1 + len2]
-	for pos < uint(len(buffer)) {
+	buffer = buffer[:cap(buffer)]
+	for {
 		if s1 < s2 {
 			buffer[pos] = s1
 			pos++
 			k1++
-			if k1 >= len1 {
+			if k1 >= len(set1) {
 				copy(buffer[pos:], set2[k2:])
-				pos += len2 - k2
-				return int(pos)
+				pos += len(set2) - k2
+				break
 			}
 			s1 = set1[k1]
 		} else if s1 == s2 {
@@ -38,14 +131,14 @@ func union2by2(set1 []uint16, set2 []uint16, buffer []uint16) int {
 			pos++
 			k1++
 			k2++
-			if k1 >= len1 {
+			if k1 >= len(set1) {
 				copy(buffer[pos:], set2[k2:])
-				pos += len2 - k2
+				pos += len(set2) - k2
 				break
 			}
-			if k2 >= len2 {
+			if k2 >= len(set2) {
 				copy(buffer[pos:], set1[k1:])
-				pos += len1 - k1
+				pos += len(set1) - k1
 				break
 			}
 			s1 = set1[k1]
@@ -54,13 +147,13 @@ func union2by2(set1 []uint16, set2 []uint16, buffer []uint16) int {
 			buffer[pos] = s2
 			pos++
 			k2++
-			if k2 >= len2 {
+			if k2 >= len(set2) {
 				copy(buffer[pos:], set1[k1:])
-				pos += len1 - k1
+				pos += len(set1) - k1
 				break
 			}
 			s2 = set2[k2]
 		}
 	}
-	return int(pos)
+	return pos
 }
--- setutil_test.go
+++ setutil_test.go
@@ -3,8 +3,11 @@ package roaring
 // to run just these tests: go test -run TestSetUtil*
 
 import (
-	"github.com/stretchr/testify/assert"
+	"math/rand"
+	"sort"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestSetUtilDifference(t *testing.T) {
@@ -41,6 +44,18 @@ func TestSetUtilDifference(t *testing.T) {
 	assert.Equal(t, expectedresult, result)
 }
 
+func TestCompareuint16(t *testing.T) {
+	assert.Equal(t, 0, compareuint16(42, 42))
+	assert.Equal(t, 1, compareuint16(1, 42))
+	assert.Equal(t, 2, compareuint16(42, 1))
+}
+
+func TestCompareuint(t *testing.T) {
+	assert.Equal(t, 0, compareuint(42, 42))
+	assert.Equal(t, 1, compareuint(1, 42))
+	assert.Equal(t, 2, compareuint(42, 1))
+}
+
 func TestSetUtilUnion(t *testing.T) {
 	data1 := []uint16{0, 1, 2, 3, 4, 9}
 	data2 := []uint16{2, 3, 4, 5, 8, 9, 11}
@@ -52,6 +67,22 @@ func TestSetUtilUnion(t *testing.T) {
 	assert.Equal(t, expectedresult, result)
 
 	nl = union2by2(data2, data1, result)
+	result = result[:nl]
+
+	assert.Equal(t, expectedresult, result)
+}
+
+func TestSetUtilUnion_branchless(t *testing.T) {
+	data1 := []uint16{0, 1, 2, 3, 4, 9}
+	data2 := []uint16{2, 3, 4, 5, 8, 9, 11}
+	result := make([]uint16, 0, len(data1)+len(data2))
+	expectedresult := []uint16{0, 1, 2, 3, 4, 5, 8, 9, 11}
+	nl := union2by2_branchless(data1, data2, result)
+	result = result[:nl]
+
+	assert.Equal(t, expectedresult, result)
+
+	nl = union2by2_branchless(data2, data1, result)
 	result = result[:nl]
 
 	assert.Equal(t, expectedresult, result)
@@ -135,4 +166,74 @@ func TestSetUtilBinarySearch(t *testing.T) {
 			assert.Equal(t, -int(key)/2-2, loc)
 		}
 	}
+}
+
+// go test  -bench BenchmarkUnion2by2 -run -
+func BenchmarkUnion2by2(b *testing.B) {
+	commonseed := 123456
+	r := rand.New(rand.NewSource(int64(commonseed)))
+
+	sarrsnum := 1024
+	sz1 := 1024
+	sarrs := make([][]uint16, sarrsnum)
+	for i := 0; i < sarrsnum; i++ {
+		sarrs[i] = make([]uint16, sz1)
+		for j := 0; j < sz1; j++ {
+			sarrs[i][j] = uint16(r.Intn(MaxUint16))
+		}
+		sort.Sort(uint16Slice(sarrs[i]))
+	}
+
+	sz2 := 1024
+	s2 := make([]uint16, sz2)
+
+	sz3 := 1024
+	s3 := make([]uint16, sz3)
+
+	sz4 := 1024
+	s4 := make([]uint16, sz4)
+
+	// We are going to populate our arrays with
+	// random data. Importantly, we need to sort.
+	// There might be a few duplicates, by random chance,
+	// but it should not affect results too much.
+
+	for i := 0; i < sz2; i++ {
+		s2[i] = uint16(r.Intn(MaxUint16))
+	}
+	sort.Sort(uint16Slice(s2))
+
+	for i := 0; i < sz3; i++ {
+		s3[i] = uint16(r.Intn(MaxUint16))
+	}
+	sort.Sort(uint16Slice(s3))
+
+	for i := 0; i < sz4; i++ {
+		s4[i] = uint16(r.Intn(MaxUint16))
+	}
+	sort.Sort(uint16Slice(s4))
+
+	buf := make([]uint16, sz1+sz2+sz3+sz4)
+
+	b.Run("union2by2_branchless", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for i := 0; i < sarrsnum; i++ {
+				union2by2_branchless(sarrs[i], s2, buf)
+				union2by2_branchless(sarrs[i], s3, buf)
+				union2by2_branchless(sarrs[i], s4, buf)
+			}
+		}
+	})
+
+	b.Run("union2by2", func(b *testing.B) {
+		b.ResetTimer()
+		for i := 0; i < b.N; i++ {
+			for i := 0; i < sarrsnum; i++ {
+				union2by2(sarrs[i], s2, buf)
+				union2by2(sarrs[i], s3, buf)
+				union2by2(sarrs[i], s4, buf)
+			}
+		}
+	})
 }
